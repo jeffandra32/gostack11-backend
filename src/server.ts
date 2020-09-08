b19c455c16131d13cbae60465761867bc04a1664
jeffandra32 <jef_xtz@yahoo.com.br>
2020-09-08 07:52:33 -0300

    feat: Add primeira rota

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
 import express, { request, response } from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({
-    name: "Jefferson",
-  });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Servidor voando 🚀 ");
